12d4d68b7bf0c0300586f81cea6147b3ac82136c
Jiangliquan <Jliquan32@163.com>
2019-09-12 17:03:21 +0800

    组件框架拆分，header在app.vue中的绝对路径引用有错，回家再解决

--- src/App.vue
+++ src/App.vue
@@ -1,23 +1,22 @@
 <template>
   <div id="app">
-    <img src="./assets/logo.png">
-    <router-view/>
+    <v-header></v-header>
+    <div class="content">
+      I am content
+    </div>
   </div>
 </template>
 
 <script>
+import header from 'components/header'
 export default {
-  name: 'App'
+  components: {
+    'v-header': header
+  }
 }
+
 </script>
 
 <style>
-#app {
-  font-family: 'Avenir', Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
-}
+
 </style>
