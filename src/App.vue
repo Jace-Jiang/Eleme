8b475b3218eefa40428c5a14a547effd6dfba677
Jace-Jiang <2060841093@qq.com>
2019-09-20 17:43:03 +0800

    组件注册未成功

--- src/App.vue
+++ src/App.vue
@@ -1,6 +1,5 @@
 <template>
   <div id="app">
-    <v-header></v-header>
     <div class="content">
         I am content
     </div>
@@ -8,30 +7,12 @@
 </template>
 
 <script>
-import header from '_c/components/header'
 export default {
   components: {
-    'v-header': header
   }
 }
 </script>
 
 <style lang="less">
-#app {
-  font-family: 'Avenir', Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-}
-#nav {
-  padding: 30px;
-  a {
-    font-weight: bold;
-    color: #2c3e50;
-    &.router-link-exact-active {
-      color: #42b983;
-    }
-  }
-}
+
 </style>
